948ea491f142a1e09e59d0172e9086b73c0702af
BurningJump <935646729@qq.com>
2018-04-28 19:35:27 +0800

    增加关卡图片

--- server/src/config/router.js
+++ server/src/config/router.js
@@ -1,8 +1,9 @@
 export default [
     // 注册登录
-    [/\/api\/index?/, 'api/index', 'rest'],
+    // [/\/api\/index?/, 'api/index', 'rest'],
     ['api/user/logout', 'post'],
-    ['api/user/register', 'rest'],
-    [/\/api\/user\/(\w+)(?:\/(\d+))?/, 'api/user/:1', 'rest'],
-    [/\/api\/common\/(\w+)(?:\/(\d+))?/, 'api/common/:1?id=:2', 'rest']
+    ['api/user/login', 'post'],
+    ['api/user/register', 'post'],
+    // [/\/api\/user\/(\w+)(?:\/(\d+))?/, 'api/user/:1', 'rest'],
+    // [/\/api\/common\/(\w+)(?:\/(\d+))?/, 'api/common/:1?id=:2', 'rest']
 ];
